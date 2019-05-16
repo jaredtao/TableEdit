e7ae0f1a3ff74041b8bfeaca7a7ad7fa981823af
jiawentao <wentaojia2014@163.com>
2019-05-16 13:41:36 +0800

    update deploy script

--- scripts/ubuntu/deploy.sh
+++ scripts/ubuntu/deploy.sh
@@ -1,3 +1,6 @@
-#! /bin/bash
+﻿#! /bin/bash
+wget https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage
+mv linuxdeployqt-6-x86_64.AppImage linuxdeployqt.AppImage
+chmod a+x linuxdeployqt.AppImage
 ./linuxdeployqt.AppImage bin/TableEdit -qmake="/opt/qt512/bin/qmake" -qmldir="/opt/qt512/qml" -appimage
 move bin/TableEdit.AppImage bin/TableEdit_ubuntu_xenial_x64.AppImage
--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -1,8 +1,6 @@
-#! /bin/bash
+﻿#! /bin/bash
 sudo add-apt-repository ppa:beineri/opt-qt-5.12.3-xenial -y
 sudo apt-get update -qq
 sudo apt-get install -y libglew-dev libglfw3-dev
 sudo apt-get install -y qt512-meta-minimal
-wget https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage
-mv linuxdeployqt-6-x86_64.AppImage linuxdeployqt.AppImage
-chmod +x linuxdeployqt.AppImage
+
